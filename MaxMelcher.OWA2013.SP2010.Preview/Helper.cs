583a87228d0907700237f991c3b9e44044348242
MaxMelcher <melcher.maximilian@gmail.com>
2013-11-23 00:47:34 +0100

    Initial commit - this is DEMONSTRATION code only. DONT DEPLOY THIS IN A PRODUCTION ENVIRONMENT - IT WONT WORK AS IS.

---
+++ MaxMelcher.OWA2013.SP2010.Preview/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+
+namespace MaxMelcher.OWA2013.SP2010.Preview
+{
+    class Helper
+    {
+        public static string GetHash(string src, string userLogin)
+        {
+            const string salt = "Max Melcher SP2010 Preview Awesomeness";
+            SHA256Managed hashstring = new SHA256Managed();
+            var crypto = hashstring.ComputeHash(Encoding.UTF8.GetBytes(src + salt + userLogin));
+            string hash = crypto.Aggregate(string.Empty, (current, bit) => current + bit.ToString("x2"));
+            return hash;
+        }
+    }
+}
---
+++ MaxMelcher.OWA2013.SP2010.Preview/SP2010Redirect.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration.Claims;
+using Microsoft.SharePoint.IdentityModel;
+using Microsoft.SharePoint.IdentityModel.OAuth2;
+using Microsoft.SharePoint.Utilities;
+
+namespace MaxMelcher.OWA2013.SP2010.Preview
+{
+    public class SP2010Redirect : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            var param = context.Request.Params;
+            string src = param["src"];
+            
+            SPUser user = SPContext.Current.Web.CurrentUser;
+
+            string login = null;
+            SPClaimProviderManager mgr = SPClaimProviderManager.Local;
+            if (mgr != null)
+            {
+                login = mgr.DecodeClaim(user.LoginName).Value;
+            }
+
+            var hash = Helper.GetHash(src, login);
+
+            //todo add a time to live to the hash and the redirect url
+
+            const string urlPreviewFormat = "http://sharepoint2013:1234/_vti_bin/sp2010preview.ashx?src={0}&user={1}&hash={2}";
+            string urlPreview = string.Format(urlPreviewFormat, src, HttpUtility.UrlEncode(login), hash);
+
+            const string owaUrlFormat = "http://owa2013.demo.com/op/embed.aspx?src={0}&action=interactivepreview";
+            string owaUrl = string.Format(owaUrlFormat, HttpUtility.UrlEncode(urlPreview));
+
+            context.Response.Redirect(owaUrl);
+        }
+
+        
+
+        public bool IsReusable { get { return false; } }
+    }
+}
